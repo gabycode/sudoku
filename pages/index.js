714b7216899e5785528171e64d7191b14b775e52
gabycode <86427446+gabycode@users.noreply.github.com>
2023-06-12 11:42:13 -0400

    add: index.js

---
+++ pages/index.js
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import SudokuGrid from "../components/SudokuGrid";
+import Numbers from "../components/Numbers";
+
+export default function Home() {
+  const [errors, setErrors] = useState(0);
+  const [selectedNumber, setSelectedNumber] = useState(null);
+  const [board, setBoard] = useState([
+    "--74916-5",
+    "2---6-3-9",
+    "-----7-1-",
+    "-586----4",
+    "--3----9-",
+    "--62--187",
+    "9-4-7---2",
+    "67-83----",
+    "81--45---",
+  ]);
+
+  return (
+    <div>
+      <h1>Sudoku</h1>
+      <hr />
+      <h2 className="errors">{errors}</h2>
+      <SudokuGrid board={board} />
+      <Numbers />
+    </div>
+  );
+}
