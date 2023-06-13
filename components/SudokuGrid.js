1ac2c1f59913781edee747f8a280e7f00b77b186
gabycode <86427446+gabycode@users.noreply.github.com>
2023-06-13 08:54:38 -0400

    add: updateTile function

--- components/SudokuGrid.js
+++ components/SudokuGrid.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-export default function SudokuGrid({ board }) {
+export default function SudokuGrid({ board, setErrors, selectedNumber }) {
   const solution = [
     "387491625",
     "241568379",
@@ -13,23 +13,52 @@ export default function SudokuGrid({ board }) {
     "812945763",
   ];
 
-  const [tiles, setTiles] = useState([]);
+  const [userBoard, setUserBoard] = useState(board);
 
-  useEffect(() => {
-    const newTiles = [];
-    for (let r = 0; r < 9; r++) {
-      for (let c = 0; c < 9; c++) {
-        const tileId = r.toString() + "-" + c.toString();
-        const tileValue = board[r][c] === "-" ? "" : board[r][c];
-        newTiles.push(
-          <div key={tileId} id={tileId} className="tile">
-            {tileValue}
-          </div>
-        );
+  const updateTile = (row, col) => {
+    if (selectedNumber !== null) {
+      const tileValue = userBoard[row][col];
+      if (tileValue !== "-") {
+        return;
+      }
+
+      const currentSolutionValue = solution[row][col];
+      if (selectedNumber.toString() === currentSolutionValue) {
+        const updatedUserBoard = [...userBoard];
+        updatedUserBoard[row] =
+          updatedUserBoard[row].substring(0, col) +
+          selectedNumber.toString() +
+          updatedUserBoard[row].substring(col + 1);
+        setUserBoard(updatedUserBoard);
+      } else {
+        setErrors((prevErrors) => prevErrors + 1);
       }
     }
-    setTiles(newTiles);
-  }, []);
+  };
+
+  const tiles = [];
+  for (let r = 0; r < 9; r++) {
+    for (let c = 0; c < 9; c++) {
+      const tileId = r.toString() + "-" + c.toString();
+      const tileValue = userBoard[r][c] === "-" ? "" : userBoard[r][c];
+      const isInitialNumber = board[r][c] !== "-";
+      const isSecondOrFifthColumn = c === 2 || c === 5;
+      const isSecondOrFifthRow = r === 2 || r === 5;
+      const tileClassName = `tile${isInitialNumber ? " tile-start" : ""}${
+        isSecondOrFifthColumn ? " vertical-line" : ""
+      }${isSecondOrFifthRow ? " horizontal-line" : ""}`;
+
+      tiles.push(
+        <div
+          key={tileId}
+          id={tileId}
+          className={tileClassName}
+          onClick={() => updateTile(r, c)}>
+          {tileValue}
+        </div>
+      );
+    }
+  }
 
   return <div className="board">{tiles}</div>;
 }
